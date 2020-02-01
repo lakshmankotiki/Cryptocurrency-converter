fb10526d3330d36ea2ce1e5b24b7131b64cc34bd
lakshmankotiki <kotiki.lakshman1996@gmail.com>
2020-02-01 20:50:26 +0530

    added html form and basic http request

--- server.js
+++ server.js
@@ -1,11 +1,28 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var request = require('request');
 var app = express();
 
+var baseURL = "https://apiv2.bitcoinaverage.com/indices/global/ticker/"
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.get('/', function(req, res) {
-    res.send("Hello World");
+    // res.send("Hello World");
+    res.sendFile(__dirname + '/public/index.html');
 });
+
+app.post('/', function(req, res) {
+    // console.log(req.body);
+    var crypto = req.body.crypto;
+    var fiat = req.body.fiat;
+    var URLFin = baseURL + crypto + fiat;
+    request(URLFin, function(error, response, body) {
+        console.log("response is: ", response);
+        console.log("body is: ", body);
+    });
+})
 
 var port = 3000;
 app.listen(port, function() {
-    console.log("application is running on port" + port);
+    console.log("application is running on port " + port);
 });
